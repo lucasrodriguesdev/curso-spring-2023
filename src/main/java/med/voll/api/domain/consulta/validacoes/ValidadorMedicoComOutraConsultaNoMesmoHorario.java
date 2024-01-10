e9b3417a1cff3fafa117f15de9e5518301bbb71c
Lucas Rodrigues <lucas.rbit@gmail.com>
2024-01-10 08:31:41 -0300

    validadores de regra de negocio implementado conforme necessidade e alguns fix de fluxo de agendamento de consulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exceptions.ValidacaoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsulta{
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var medicoPossuiOutraConsultaNoMesmoHorario = consultaRepository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+        if (medicoPossuiOutraConsultaNoMesmoHorario){
+            throw new ValidacaoException("Medico ja possui outra consulta agendada neste mesmo horario");
+        }
+    }
+}
---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteAtivo.java
@@ -0,0 +1,28 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.infra.exceptions.ValidacaoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ValidadorPacienteAtivo implements ValidadorAgendamentoDeConsulta{
+
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        if(dados.idPaciente() == null){
+            return;
+        }
+        var pacienteEstaAtivo = pacienteRepository.findAtivoById(dados.idPaciente());
+        if (!pacienteEstaAtivo){
+            throw new ValidacaoException("Consulta nao pode ser agendada com médico inativo");
+        }
+
+    }
+}
